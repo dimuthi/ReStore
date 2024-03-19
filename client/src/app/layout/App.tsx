0199320f84365f48aac9196d573a376e390c05fe
Pramodya Abeysinghe <dimuthi@gmail.com>
2024-03-19 12:31:43 -0600

    toast notification added

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,7 +2,8 @@ import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/materia
 import Header from "./Header";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 // const products = [
 //   {name:'product1', price:100.00},
 //   {name:'product2', price:200.00}
@@ -31,6 +32,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container><Outlet /></Container>

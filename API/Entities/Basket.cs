04a27a0abef0d196bd043e74fe7b195a12fef2e0
Pramodya Abeysinghe <dimuthi@gmail.com>
2024-03-21 22:14:15 -0600

    Basket dbset added to the context

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -12,6 +12,23 @@ namespace API.Entities
             if(Items.All(item => item.ProdductId != product.Id)){
                 Items.Add(new BasketItem{Product = product, Quantity = quantity});
             }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if(existingItem!=nul){
+                existingItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity){
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if(item == null) {
+                return;
+            }
+            item.Quantity -= quantity;
+            if(item.Quantity == 0) {
+                Items.Remove(item)
+            }
+
         }
     }
 }
